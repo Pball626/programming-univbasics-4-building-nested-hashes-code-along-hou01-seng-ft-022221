33446d5bb50eec89fcae35d44a23859f5a1db582
Jose Mendoza <pball626@gmail.com>
2021-02-08 04:10:59 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,6 +1,6 @@
 
 def first_challenge
-epic_tragedy = {
+epic_tragedy = {[
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
