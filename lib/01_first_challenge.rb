9e0e88cc960286c03771cf68c2aecd8f18265ed3
Jose Mendoza <pball626@gmail.com>
2021-02-08 03:45:02 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -12,7 +12,8 @@ def first_challenge
        :hero => {name: "Juliet", age:"15", status: "alive"}
        :hero_friends => [
          {:name "Benvolio", age: "17", :attitude, "worried"}
-       {:}]
+       {:name "Mercutio", age:"18", :attitude, "hot-headed"}
+       ]
      }
    }
 end
