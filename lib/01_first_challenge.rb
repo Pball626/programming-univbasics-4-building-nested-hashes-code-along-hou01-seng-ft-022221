5860c46c149aa14f6336305548a1325b60542b58
Jose Mendoza <pball626@gmail.com>
2021-02-08 03:43:19 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -11,8 +11,8 @@ def first_challenge
        :matriarch => {name: "Lady Capulet", age: "51"},
        :hero => {name: "Juliet", age:"15", status: "alive"}
        :hero_friends => [
-         {:name "Benvolio", age: "17", :attitude, "worried"
-       }]
+         {:name "Benvolio", age: "17", :attitude, "worried"}
+       {:}]
      }
    }
 end
